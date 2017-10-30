eab08c7ff172a88e66141cc187a68a83210dd2f9
Leandro Zambelli <leandro.zambelli@hotmail.com>
2017-10-30 12:56:10 -0200

    changes

---
+++ js/functions.js
@@ -0,0 +1,40 @@
+function navMobile() {
+    
+    var
+        $nav_mobile     = document.getElementById('nav-mobile'),
+        $btn_toggle_nav = document.getElementsByClassName('btn-toggle-nav')[0];
+
+    function toggleNav() {
+        
+        if ($nav_mobile.style.maxHeight) {
+            $nav_mobile.style.maxHeight = null;
+        } else {
+            $nav_mobile.style.maxHeight = $nav_mobile.scrollHeight + "px";
+        }
+        
+        $btn_toggle_nav.classList.toggle('bar-close');
+    }
+
+    $btn_toggle_nav.addEventListener('click', toggleNav);
+}
+
+navMobile();
+
+
+function backTopButton() {
+    
+    var
+        $back_top = document.getElementsByClassName('back-top')[0],
+        i;
+    
+    window.onscroll = function scrollFunction() {
+        
+        if (document.documentElement.scrollTop > 100) {
+            $back_top.classList.remove('hide-me');
+        } else {
+            $back_top.classList.add('hide-me');
+        }
+    }
+}
+
+backTopButton();
